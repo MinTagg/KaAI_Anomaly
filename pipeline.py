176b5bfafddc819d07600d990337c1e2c32cfc80
MinTagg <98318559+MinTagg@users.noreply.github.com>
2022-12-29 17:23:21 +0900

    Add files via upload

--- pipeline.py
+++ pipeline.py
@@ -6,37 +6,6 @@ import json
 # 해당 클래스는 데이터 제너레이터
 # fit 함수 안에 클래스로 넣으면 작동할 듯
 class ano_dataset(tf.data.Dataset):
-    def __init__(self):
-        print('Hello World')
-        
-        print(self.df.type)
-        self.length = len(self.df)
-
-    def load_json(self, NAME):
-        print('Shuffle Data Frame')
-        margin = (arg.model_input-1)//2
-        #result = [0 for _ in range(arg.object_count*arg.model_input)] # 반환하기 위한 빈 리스트
-        result = []
-
-        for i in range(arg.model_input):
-            temp_result = []
-            json_path = f'{arg.json_path+str(int(NAME)-margin+i)}.json' # 열 json 파일의 주소
-            print(f'Opened Json :: {str(int(NAME)-margin+i)}')
-            with open(json_path, 'r') as f:
-                data = json.load(f)
-            for i in range(len(data['xmin'])):
-                temp_result.append(int(data['xmin'][str(i)]))
-                temp_result.append(int(data['ymin'][str(i)]))
-                temp_result.append(int(data['xmax'][str(i)]))
-                temp_result.append(int(data['ymax'][str(i)]))
-                if len(temp_result) >= arg.object_count: # 10개의 오브젝트만 확인한다
-                    break
-            while len(temp_result) != arg.object_count: # 10개의 오브젝트보다 작다면 0을 추가한다
-                temp_result.append(0)
-            result = result + temp_result
-            
-        return result
-
     def _generator(self):
         def load_json( NAME):
             
@@ -47,7 +16,7 @@ class ano_dataset(tf.data.Dataset):
             for i in range(arg.model_input):
                 temp_result = []
                 json_path = f'{arg.json_path+str(int(NAME)-margin+i)}.json' # 열 json 파일의 주소
-                print(f'Opened Json :: {str(int(NAME)-margin+i)}')
+                #print(f'Opened Json :: {str(int(NAME)-margin+i)}')
                 with open(json_path, 'r') as f:
                     data = json.load(f)
                 for i in (data['xmin'].keys()):
@@ -67,7 +36,7 @@ class ano_dataset(tf.data.Dataset):
         df = df.sample(frac=1) # 데이터 프레임 셔플
         for _, row in df.iterrows():
             _, name = row['Path'].split('/')
-            print(f'Middle Name :: {name}')
+            #print(f'Middle Name :: {name}')
             result = load_json(name[:-4]) # bbox정보가 합쳐져서 들어온다. list 형식으로 길이는 40*5. 과거부터 들어온다
             print('')
             yield (result)
